5766741d047a9619e21b4c5f0da34489c6c119ab
djsiclait <djidjellypas92@gmail.com>
2019-10-01 20:21:52 +0200

    Edit create Telefono cross table script

--- Database Structure/Telefono/Create Telefono.sql
+++ Database Structure/Telefono/Create Telefono.sql
@@ -1,7 +1,7 @@
 Create Table Tbl_Telefono
 (
-	fld_cod_cli varchar(50) Not Null, -- codigo de membresia del cliente
-	fld_tel_cli varchar(30) Default '', -- telefono del cliente
-	fld_tip_tel varchar(3) Default 'CEL', -- tipo de telefono del cliente
-	fld_est_tel varchar(1) Default 'A' -- estado del telefono del cliente
+	fld_cod_cli varchar(50) Not Null, -- codigo de membresia del cliente y usuario
+	fld_tel_cli varchar(30) Default '', -- telefono del cliente y usuarios
+	fld_tip_tel varchar(3) Default 'CEL', -- tipo de telefono del cliente y usuario; TRA solo disponible para los clientes
+	fld_est_tel varchar(1) Default 'A' -- estado del telefono del cliente y usuario
 )
