6a4a807d2cfd8b7037a64c25be61ad0d646ac831
djsiclait <djidjellypas92@gmail.com>
2019-10-02 10:46:19 -0300

    Create basic design for main menu page

--- NetFlics/NetFlics/FormMainMenu.Designer.cs
+++ NetFlics/NetFlics/FormMainMenu.Designer.cs
@@ -29,7 +29,190 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMainMenu));
+            this.bttAlquiler = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bttInventario = new System.Windows.Forms.Button();
+            this.bttClientes = new System.Windows.Forms.Button();
+            this.bttReportes = new System.Windows.Forms.Button();
+            this.bttUsuarios = new System.Windows.Forms.Button();
+            this.bttSalir = new System.Windows.Forms.Button();
+            this.bttPeliculas = new System.Windows.Forms.Button();
+            this.bttFacturas = new System.Windows.Forms.Button();
+            this.bttConfig = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // bttAlquiler
+            // 
+            this.bttAlquiler.BackColor = System.Drawing.Color.Silver;
+            this.bttAlquiler.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttAlquiler.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttAlquiler.ForeColor = System.Drawing.Color.Red;
+            this.bttAlquiler.Image = ((System.Drawing.Image)(resources.GetObject("bttAlquiler.Image")));
+            this.bttAlquiler.Location = new System.Drawing.Point(78, 55);
+            this.bttAlquiler.Name = "bttAlquiler";
+            this.bttAlquiler.Size = new System.Drawing.Size(100, 100);
+            this.bttAlquiler.TabIndex = 0;
+            this.bttAlquiler.Text = "Alquiler";
+            this.bttAlquiler.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.bttAlquiler.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(126, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(224, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Obsoleto pero Nunca Olvidado";
+            // 
+            // bttInventario
+            // 
+            this.bttInventario.BackColor = System.Drawing.Color.DarkRed;
+            this.bttInventario.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttInventario.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttInventario.ForeColor = System.Drawing.Color.Silver;
+            this.bttInventario.Image = ((System.Drawing.Image)(resources.GetObject("bttInventario.Image")));
+            this.bttInventario.Location = new System.Drawing.Point(184, 55);
+            this.bttInventario.Name = "bttInventario";
+            this.bttInventario.Size = new System.Drawing.Size(100, 100);
+            this.bttInventario.TabIndex = 2;
+            this.bttInventario.Text = "Inventario";
+            this.bttInventario.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.bttInventario.UseVisualStyleBackColor = false;
+            this.bttInventario.Click += new System.EventHandler(this.bttInventario_Click);
+            // 
+            // bttClientes
+            // 
+            this.bttClientes.BackColor = System.Drawing.Color.Silver;
+            this.bttClientes.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttClientes.ForeColor = System.Drawing.Color.Red;
+            this.bttClientes.Image = ((System.Drawing.Image)(resources.GetObject("bttClientes.Image")));
+            this.bttClientes.Location = new System.Drawing.Point(290, 55);
+            this.bttClientes.Name = "bttClientes";
+            this.bttClientes.Size = new System.Drawing.Size(100, 100);
+            this.bttClientes.TabIndex = 3;
+            this.bttClientes.Text = "Clientes";
+            this.bttClientes.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.bttClientes.UseVisualStyleBackColor = false;
+            // 
+            // bttReportes
+            // 
+            this.bttReportes.BackColor = System.Drawing.Color.Silver;
+            this.bttReportes.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttReportes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttReportes.ForeColor = System.Drawing.Color.Red;
+            this.bttReportes.Image = ((System.Drawing.Image)(resources.GetObject("bttReportes.Image")));
+            this.bttReportes.Location = new System.Drawing.Point(184, 161);
+            this.bttReportes.Name = "bttReportes";
+            this.bttReportes.Size = new System.Drawing.Size(100, 100);
+            this.bttReportes.TabIndex = 4;
+            this.bttReportes.Text = "Reportes";
+            this.bttReportes.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.bttReportes.UseVisualStyleBackColor = false;
+            // 
+            // bttUsuarios
+            // 
+            this.bttUsuarios.BackColor = System.Drawing.Color.Silver;
+            this.bttUsuarios.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttUsuarios.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttUsuarios.ForeColor = System.Drawing.Color.Red;
+            this.bttUsuarios.Image = ((System.Drawing.Image)(resources.GetObject("bttUsuarios.Image")));
+            this.bttUsuarios.Location = new System.Drawing.Point(78, 264);
+            this.bttUsuarios.Name = "bttUsuarios";
+            this.bttUsuarios.Size = new System.Drawing.Size(100, 100);
+            this.bttUsuarios.TabIndex = 5;
+            this.bttUsuarios.Text = "Usuarios";
+            this.bttUsuarios.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.bttUsuarios.UseVisualStyleBackColor = false;
+            // 
+            // bttSalir
+            // 
+            this.bttSalir.BackColor = System.Drawing.Color.Silver;
+            this.bttSalir.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttSalir.ForeColor = System.Drawing.Color.Red;
+            this.bttSalir.Image = ((System.Drawing.Image)(resources.GetObject("bttSalir.Image")));
+            this.bttSalir.Location = new System.Drawing.Point(290, 267);
+            this.bttSalir.Name = "bttSalir";
+            this.bttSalir.Size = new System.Drawing.Size(100, 100);
+            this.bttSalir.TabIndex = 6;
+            this.bttSalir.Text = "Salir";
+            this.bttSalir.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.bttSalir.UseVisualStyleBackColor = false;
+            // 
+            // bttPeliculas
+            // 
+            this.bttPeliculas.BackColor = System.Drawing.Color.DarkRed;
+            this.bttPeliculas.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttPeliculas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttPeliculas.ForeColor = System.Drawing.Color.Silver;
+            this.bttPeliculas.Image = ((System.Drawing.Image)(resources.GetObject("bttPeliculas.Image")));
+            this.bttPeliculas.Location = new System.Drawing.Point(78, 161);
+            this.bttPeliculas.Name = "bttPeliculas";
+            this.bttPeliculas.Size = new System.Drawing.Size(100, 100);
+            this.bttPeliculas.TabIndex = 7;
+            this.bttPeliculas.Text = "Peliculas";
+            this.bttPeliculas.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.bttPeliculas.UseVisualStyleBackColor = false;
+            // 
+            // bttFacturas
+            // 
+            this.bttFacturas.BackColor = System.Drawing.Color.DarkRed;
+            this.bttFacturas.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttFacturas.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttFacturas.ForeColor = System.Drawing.Color.Silver;
+            this.bttFacturas.Image = ((System.Drawing.Image)(resources.GetObject("bttFacturas.Image")));
+            this.bttFacturas.Location = new System.Drawing.Point(290, 161);
+            this.bttFacturas.Name = "bttFacturas";
+            this.bttFacturas.Size = new System.Drawing.Size(100, 100);
+            this.bttFacturas.TabIndex = 8;
+            this.bttFacturas.Text = "Facturas";
+            this.bttFacturas.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.bttFacturas.UseVisualStyleBackColor = false;
+            // 
+            // bttConfig
+            // 
+            this.bttConfig.BackColor = System.Drawing.Color.DarkRed;
+            this.bttConfig.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bttConfig.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bttConfig.ForeColor = System.Drawing.Color.Silver;
+            this.bttConfig.Image = ((System.Drawing.Image)(resources.GetObject("bttConfig.Image")));
+            this.bttConfig.Location = new System.Drawing.Point(184, 267);
+            this.bttConfig.Name = "bttConfig";
+            this.bttConfig.Size = new System.Drawing.Size(100, 100);
+            this.bttConfig.TabIndex = 9;
+            this.bttConfig.Text = "Config";
+            this.bttConfig.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.bttConfig.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 396);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(54, 53);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Silver;
+            this.label2.Location = new System.Drawing.Point(58, 419);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "etFlics";
             // 
             // FormMainMenu
             // 
@@ -38,14 +221,41 @@
             this.BackColor = System.Drawing.Color.LightGray;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.bttConfig);
+            this.Controls.Add(this.bttFacturas);
+            this.Controls.Add(this.bttPeliculas);
+            this.Controls.Add(this.bttSalir);
+            this.Controls.Add(this.bttUsuarios);
+            this.Controls.Add(this.bttReportes);
+            this.Controls.Add(this.bttClientes);
+            this.Controls.Add(this.bttInventario);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bttAlquiler);
             this.MaximizeBox = false;
             this.Name = "FormMainMenu";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "NetFlics v1.0";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button bttAlquiler;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bttInventario;
+        private System.Windows.Forms.Button bttClientes;
+        private System.Windows.Forms.Button bttReportes;
+        private System.Windows.Forms.Button bttUsuarios;
+        private System.Windows.Forms.Button bttSalir;
+        private System.Windows.Forms.Button bttPeliculas;
+        private System.Windows.Forms.Button bttFacturas;
+        private System.Windows.Forms.Button bttConfig;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- NetFlics/NetFlics/FormMainMenu.cs
+++ NetFlics/NetFlics/FormMainMenu.cs
@@ -16,5 +16,10 @@ namespace NetFlics
         {
             InitializeComponent();
         }
+
+        private void bttInventario_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
