4da0d59952d1ece1f0239c90c57559561ca09a0a
djsiclait <djidjellypas92@gmail.com>
2019-10-02 11:24:14 -0300

    Create UserSession class to track users and role access

---
+++ NetFlics/CapaEntidad/UserSession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class UserSession // global class to monitor user session 
+    {
+        public static string username { get; set; } // username 
+        public static string firstName { get; set; } // name of euser in session
+        public static string lastName { get; set; } // lastname of user in session
+        public static string role { get; set; } // user pre defined role 
+    }
+}
