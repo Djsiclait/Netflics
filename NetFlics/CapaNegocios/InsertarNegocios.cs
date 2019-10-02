21a7b803a156e6b36ba76fcc751ec18189200606
djsiclait <djidjellypas92@gmail.com>
2019-10-01 21:27:56 -0300

    Design Splash page

--- NetFlics/CapaNegocios/InsertarNegocios.cs
+++ NetFlics/CapaNegocios/InsertarNegocios.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CapaNegocios
 {
-    class InsertarNegocio
+    class InsertarNegocios
     {
     }
 }
--- NetFlics/NetFlics/FormSplash.cs
+++ NetFlics/NetFlics/FormSplash.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace NetFlics
 {
-    public partial class Form1 : Form
+    public partial class FormSplash : Form
     {
-        public Form1()
+        public FormSplash()
         {
             InitializeComponent();
         }
