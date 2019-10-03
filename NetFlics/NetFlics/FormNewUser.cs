55da7f2a0ec599871c1417b3c0dac336a8bd01b1
djsiclait <djidjellypas92@gmail.com>
2019-10-03 16:59:14 -0400

    Create New User Form

---
+++ NetFlics/NetFlics/FormNewUser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaEntidad;
+
+namespace NetFlics
+{
+    public partial class FormNewUser : Form
+    {
+        public FormNewUser()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            string username = CapaEntidad.UserSession.userSession.username;
+            string message = CapaNegocios.ModificarNegocios.UserLogOut(username, "MAIN");
+
+            if (message == "EXITO")
+            {
+                string lastSession = CapaEntidad.UserSession.userSession.username;
+                CapaEntidad.UserSession.userSession = new UserSession(); // Clear user session data for future log in
+                CapaEntidad.UserSession.userSession.username = lastSession;
+                Formularios.formNewUser.Dispose();
+
+                // returning to login page
+                Formularios.formLogin = new FormLogin();
+                Formularios.formLogin.Show();
+            }
+            else
+                MessageBox.Show("A fatal error has occured");
+        }
+    }
+}
