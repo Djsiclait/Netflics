397a8ec9a4c5f0ecf3f1e267f778a93a6a573043
djsiclait <djidjellypas92@gmail.com>
2019-10-03 14:38:32 -0400

    Create User class in Entity layer to display user information

---
+++ NetFlics/CapaEntidad/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaEntidad
+{
+    public class User
+    {
+        public string username { get; set; }
+		public string firstName { get; set; } 
+		public string lastName { get; set; } 
+		public string email { get; set; } 
+		public string gender { get; set; }
+		public string position { get; set; }
+		public string role { get; set; }
+		public string branchOffice { get; set; }
+    }
+}
