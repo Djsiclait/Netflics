ac97fc57a616d4b8b8c563b5e05c964efc6d3fe8
djsiclait <djidjellypas92@gmail.com>
2019-10-05 10:54:42 -0400

    Modify User clas in Entity layer to include complete set of fields

--- NetFlics/CapaEntidad/User.cs
+++ NetFlics/CapaEntidad/User.cs
@@ -11,11 +11,17 @@ namespace CapaEntidad
     public class User
     {
         public string username { get; set; }
+        public string identification { get; set; }
+        public string identificationType { get; set; }
 		public string firstName { get; set; } 
 		public string lastName { get; set; } 
 		public string email { get; set; } 
 		public string gender { get; set; }
+        public DateTime birthDate { get; set; }
+        public string nationality { get; set; }
 		public string position { get; set; }
+        public float salary { get; set; }
+        public string paymentSchedule { get; set; }
 		public string role { get; set; }
 		public string branchOffice { get; set; }
     }
